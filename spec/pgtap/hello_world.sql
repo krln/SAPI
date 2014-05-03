dfcdd56cbffcdf0de6ee02aae712269ff2ea4c6f
Agnieszka Figiel <agnieszka.figiel@unep-wcmc.org>
2014-05-03 23:26:18 +0100

    switched to XUnit style

--- spec/pgtap/hello_world.sql
+++ spec/pgtap/hello_world.sql
@@ -1,10 +1,12 @@
--- Start transaction and plan the tests.
-BEGIN;
-SELECT plan(1);
-
--- Run the tests.
-SELECT pass( 'My test passed, w00t!' );
-
--- Finish the tests and clean up.
-SELECT * FROM finish();
-ROLLBACK;
+CREATE OR REPLACE FUNCTION test_hello_world(
+) RETURNS SETOF TEXT AS $$
+  WITH geo_entity_type AS (
+    INSERT INTO geo_entity_types (name, created_at, updated_at)
+    VALUES ('COUNTRY', NOW(), NOW())
+    RETURNING id
+  )
+  INSERT INTO geo_entities (name_en, iso_code2, geo_entity_type_id, created_at, updated_at)
+  SELECT 'Poland', 'PL', geo_entity_type.id, NOW(), NOW()
+  FROM geo_entity_type;
+  SELECT is( iso_code2, 'PL', 'Should have iso code 2') FROM geo_entities;
+$$ LANGUAGE sql;
